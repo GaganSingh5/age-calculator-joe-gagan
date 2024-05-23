3d67266b04a752951b362424a4b0eef5ac63fbce
joekotvas <joseph.kotvas@gmail.com>
2024-05-23 15:54:13 -0500

    first commit

---
+++ joe-gagan-age-calculator/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main className="min-h-[100vh] flex items-center justify-center">
+        <h1 className="text-bold text-2xl">App Goes Here</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
